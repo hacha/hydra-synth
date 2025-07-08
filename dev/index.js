5a7c74714306a7133ee9ac688da2c63d48b181e5
hacha <hacha.dev@gmail.com>
2025-07-08 12:50:17 +0900

    add: easing-functionの短縮形

--- dev/index.js
+++ dev/index.js
@@ -7,31 +7,52 @@ const { fugitiveGeometry, exampleVideo, exampleResize, nonGlobalCanvas } = requi
 // console.log('HYDRA', Hydra)
 // const HydraShaders = require('./../shader-generator.js')
 
-function init () {
- 
-//   const canvas = document.createElement('canvas')
-//   canvas.style.backgroundColor = "#000"
-//   canvas.width = 800
-//   canvas.height = 200
-//   document.body.appendChild(canvas)
-//   // canvas.style.width = '100%'
-//   // canvas.style.height = '100%'
-// //  exampleCustomCanvas()
+function init() {
+
+    //   const canvas = document.createElement('canvas')
+    //   canvas.style.backgroundColor = "#000"
+    //   canvas.width = 800
+    //   canvas.height = 200
+    //   document.body.appendChild(canvas)
+    //   // canvas.style.width = '100%'
+    //   // canvas.style.height = '100%'
+    // //  exampleCustomCanvas()
 
 
 
-var hydra = new Hydra({detectAudio:false, makeGlobal: true})
+    var hydra = new Hydra({ detectAudio: false, makeGlobal: true })
 
-osc().out()
-// console.log(hydra)
-// window.hydra = hydra
-// // //osc().out()
-// exampleVideo()
-// exampleResize()
-//nonGlobalCanvas()
+    // Test noise shorthand noi()
+    // noi()
+    // .modScale(vor(4, .9).thr(.6, 0), .1)
+    // .bri(-.3)
+    // .pstr(6)
+    // .rot(0, .3)
+    // .sca([1, 1.5].smooth())
+    // .pix(160, 9999)
+    // .mul(grad().scr(.0, .0, 0, .2))
+    // .cont(.84)
+    // .modRep(osc(2))
+    // .inv([0, 1])
+    // .col(1, .43, 1.93)
+    // .sat(2)
+    // .out()
 
-//s0.initVideo("https://media.giphy.com/media/26ufplp8yheSKUE00/giphy.mp4", {})
-//src(s0).repeat().out()
+    solid()
+    .add(shape(4,.1).scrY(.2).scrX([-.2,.2].eoQuint()))
+    .add(shape(4,.1).scrY(-.0).scrX([-.2,.2].eQuart()))
+    .add(shape(4,.1).scrY(-.2).scrX([-.2,.2].eiCubic()))
+    .add(shape(4,.1).scrY(-.4).scrX([-.2,.2].lin()))
+    .out()
+
+    // console.log(hydra)
+    // window.hydra = hydra
+    // exampleVideo()
+    // exampleResize()
+    //nonGlobalCanvas()
+
+    //s0.initVideo("https://media.giphy.com/media/26ufplp8yheSKUE00/giphy.mp4", {})
+    //src(s0).repeat().out()
 }
 
 window.onload = init
--- dist/hydra-synth.js
+++ dist/hydra-synth.js
@@ -3473,8 +3473,60 @@ var _default = {
 
     Array.prototype.e = Array.prototype.ease;
 
-    Array.prototype.sine = function () {
+    Array.prototype.lin = function () {
+      return this.ease('linear');
+    };
+
+    Array.prototype.sin = function () {
       return this.ease('sin');
+    };
+
+    Array.prototype.eiCubic = function () {
+      return this.ease('easeInCubic');
+    };
+
+    Array.prototype.eoCubic = function () {
+      return this.ease('easeOutCubic');
+    };
+
+    Array.prototype.eCubic = function () {
+      return this.ease('easeInOutCubic');
+    };
+
+    Array.prototype.eiQuad = function () {
+      return this.ease('easeInQuad');
+    };
+
+    Array.prototype.eoQuad = function () {
+      return this.ease('easeOutQuad');
+    };
+
+    Array.prototype.eQuad = function () {
+      return this.ease('easeInOutQuad');
+    };
+
+    Array.prototype.eiQuart = function () {
+      return this.ease('easeInQuart');
+    };
+
+    Array.prototype.eoQuart = function () {
+      return this.ease('easeOutQuart');
+    };
+
+    Array.prototype.eQuart = function () {
+      return this.ease('easeInOutQuart');
+    };
+
+    Array.prototype.eiQuint = function () {
+      return this.ease('easeInQuint');
+    };
+
+    Array.prototype.eoQuint = function () {
+      return this.ease('easeOutQuint');
+    };
+
+    Array.prototype.eQuint = function () {
+      return this.ease('easeInOutQuint');
     };
 
     Array.prototype.offset = function (offset = 0.5) {
