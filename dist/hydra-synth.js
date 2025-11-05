hacha/hydra-synth

commit 9b4e8e966cf43765b40413ae09bacbab791c54d7

removed render passes

src/output.js
@@ -8,17 +8,12 @@ var Output = function (opts) {
     [2, 2]
   ])
 
+  this.draw = () => {}
   this.clear()
   this.pingPongIndex = 0
 
-  this.renderIndex = 0
-  this.prevFrameIndex = 2
-  var alternate = 3 - (this.renderIndex + this.prevFrameIndex)
-  this.pingPong = [this.renderIndex, alternate]
-
-  // for each output, create three fbos to use for ping ponging and for
-  // storing the previous frame
-  this.fbos = (Array(3)).fill().map(() => this.regl.framebuffer({
+  // for each output, create two fbos for pingponging
+  this.fbos = (Array(2)).fill().map(() => this.regl.framebuffer({
     color: this.regl.texture({
       width: opts.width,
       height: opts.height,
@@ -47,14 +42,14 @@ Output.prototype.getCurrent = function () {
   //console.log("getting", this.prevFrameIndex)
   //return this.fbos[this.renderIndex]
 //  console.log("get current",this.prevFrameIndex)
-  return this.fbos[this.prevFrameIndex]
+  return this.fbos[this.pingPongIndex]
 }
 
 Output.prototype.getTexture = function () {
 //  return this.fbos[!this.pingPongIndex]
-  // var index = this.pingPongIndex ? 0 : 1
+   var index = this.pingPongIndex ? 0 : 1
   //  console.log("get texture",index)
-  var index = this.prevFrameIndex
+//  var index = this.prevFrameIndex
 //  console.log("get texture",index)
   return this.fbos[index]
 }
@@ -120,82 +115,92 @@ Output.prototype.clear = function () {
 //   this.frag = frag
 // }
 
-// Output.prototype.render = function () {
-//   this.draw = this.regl({
-//     frag: this.frag,
-//     vert: this.vert,
-//     attributes: this.attributes,
-//     uniforms: this.uniforms,
-//     count: 3,
-//     framebuffer: () => {
-//       this.pingPongIndex = this.pingPongIndex ? 0 : 1
-//       this.prevFrameIndex = this.renderIndex
-//       this.renderIndex++
-//       if(this.renderIndex > this.fbos.length - 1) this.renderIndex = 0
-//       return this.fbos[this.renderIndex]
-//     }
-//   })
-// }
-
-Output.prototype.renderPasses = function(passes) {
-  console.log('rendering', passes)
+Output.prototype.render = function (pass) {
+  console.log('pass', pass, this.pingPongIndex)
   var self = this
-//  console.log("passes", passes)
-
-  // values > diff > sum > desired result
-  // 0 1 > 1 > 1 > 2
-  // 1 2 > 1 > 3 > 0
-  // 2 0 > 2 > 2 > 1
-
-  // get buffer that is neither storing
-
-
-  this.passes = passes.map( (pass, passIndex) => {
-
-    //  console.log("get texture",index)
-    var uniforms = Object.assign(pass.uniforms, { prevBuffer:  () =>  {
-           //var index = this.pingPongIndex ? 0 : 1
-           var index = self.pingPong[(passIndex+1)%2]
-      //    console.log('pass index', self.pingPong, 'fbo index', index)
-         //return this.fbos[this.pingPongIndex ? 0 : 1]
-          return this.fbos[index]
-        }
-      })
-
-      return {
-        draw: self.regl({
-          frag: pass.frag,
-          vert: self.vert,
-          attributes: self.attributes,
-          uniforms: uniforms,
-          count: 3,
-          framebuffer: () => {
-          //  var prev =   self.prevFrameIndex
-          //  self.prevFrameIndex = this.renderIndex
-          //  self.renderIndex = alternate
-          //    console.log(self.prevFrameIndex, self.renderIndex)
-          //  self.pingPongIndex = self.pingPongIndex ? 0 : 1
-            var index = self.pingPong[(passIndex)%2]
-            //console.log('pass index', self.pingPong, 'render index',  pass, index)
-            return self.fbos[index]
+      var uniforms = Object.assign(pass.uniforms, { prevBuffer:  () =>  {
+             //var index = this.pingPongIndex ? 0 : 1
+          //   var index = self.pingPong[(passIndex+1)%2]
+            console.log('ping pong', self.pingPongIndex)
+            return self.fbos[self.pingPongIndex]
           }
         })
-      }
-  })
 
+  self.draw = self.regl({
+    frag: pass.frag,
+    vert: self.vert,
+    attributes: self.attributes,
+    uniforms: uniforms,
+    count: 3,
+    framebuffer: () => {
+      self.pingPongIndex = self.pingPongIndex ? 0 : 1
+      return self.fbos[self.pingPongIndex]
+    }
+  })
 }
+
+// Output.prototype.renderPasses = function(passes) {
+//   console.log('rendering', passes)
+//   var self = this
+// //  console.log("passes", passes)
+//
+//   // values > diff > sum > desired result
+//   // 0 1 > 1 > 1 > 2
+//   // 1 2 > 1 > 3 > 0
+//   // 2 0 > 2 > 2 > 1
+//
+//   // get buffer that is neither storing
+//
+//
+//   this.passes = passes.map( (pass, passIndex) => {
+//
+//     //  console.log("get texture",index)
+//     var uniforms = Object.assign(pass.uniforms, { prevBuffer:  () =>  {
+//            //var index = this.pingPongIndex ? 0 : 1
+//            var index = self.pingPong[(passIndex+1)%2]
+//       //    console.log('pass index', self.pingPong, 'fbo index', index)
+//          //return this.fbos[this.pingPongIndex ? 0 : 1]
+//           return this.fbos[index]
+//         }
+//       })
+//
+//       return {
+//         draw: self.regl({
+//           frag: pass.frag,
+//           vert: self.vert,
+//           attributes: self.attributes,
+//           uniforms: uniforms,
+//           count: 3,
+//           framebuffer: () => {
+//           //  var prev =   self.prevFrameIndex
+//           //  self.prevFrameIndex = this.renderIndex
+//           //  self.renderIndex = alternate
+//           //    console.log(self.prevFrameIndex, self.renderIndex)
+//           //  self.pingPongIndex = self.pingPongIndex ? 0 : 1
+//             var index = self.pingPong[(passIndex)%2]
+//             //console.log('pass index', self.pingPong, 'render index',  pass, index)
+//             return self.fbos[index]
+//           }
+//         })
+//       }
+//   })
+//
+// }
 
 Output.prototype.tick = function (props) {
 //  this.draw(props)
 //  console.log('render', this.renderIndex, 'previous', this.prevFrameIndex)
 //console.log('indices', this.renderIndex, this.prevFrameIndex, alternate)
-  this.passes.forEach((pass) => pass.draw(props))
+//console.log(this.passes, props)
+//  this.passes.forEach((pass) => pass.draw(props))
+console.log('tick', this)
+this.draw(props)
 //  console.log('passesp', this.prevFrameIndex, this.renderIndex
-  var prev = this.prevFrameIndex
-  this.prevFrameIndex = this.pingPong[(this.passes.length+1)%2]
-  this.renderIndex = prev
-  var alternate = 3 - (this.renderIndex + this.prevFrameIndex)
-  this.pingPong = [this.renderIndex, alternate]
+  // var prev = this.prevFrameIndex
+  // this.prevFrameIndex = this.pingPong[(this.passes.length+1)%2]
+  // this.renderIndex = prev
+  // var alternate = 3 - (this.renderIndex + this.prevFrameIndex)
+  // this.pingPong = [this.renderIndex, alternate]
   // var prev = this.prevFrameIndex
   // this.prevFrameIndex = this.renderIndex
   // console.log(this.renderIndex, prev, alternate)

example/index.js
@@ -46,16 +46,18 @@ function init () {
     }`
   })
 
-  ooo(10, 0.01, 1.2).blur().out()
+  // ooo(10, 0.01, 1.2).blur().out()
+  //
+  // // Example array sequences
+  // shape([4, 5, 3]).out()
+  //
+  // // array easing
+  // shape([4, 3, 2].ease('easeInQuad')).out()
+  //
+  // // array smoothing
+  // shape([4, 3, 2].smooth()).out()
 
-  // Example array sequences
-  shape([4, 5, 3]).out()
-
-  // array easing
-  shape([4, 3, 2].ease('easeInQuad')).out()
-
-  // array smoothing
-  shape([4, 3, 2].smooth()).out()
+  osc().out()
 
   // set bpm
   bpm(30)

index.js
@@ -343,7 +343,6 @@ class HydraSynth {
   }
 
   tick (dt, uniforms) {
-
   //  if(self.detectAudio === true) self.fft = self.audio.frequencies()
   // this.regl.frame(function () {
     this.time += dt * 0.001

src/glsl-source.js
@@ -22,8 +22,8 @@ GlslSource.prototype.out = function (_output) {
   var output = _output || this.defaultOutput
   var glsl = this.glsl(output)
   this.synth.currentFunctions = []
-  output.renderPasses(glsl)
-
+ // output.renderPasses(glsl)
+  output.render(glsl[0])
 }
 
 GlslSource.prototype.glsl = function (_output) {
@@ -51,7 +51,7 @@ GlslSource.prototype.glsl = function (_output) {
 
 
   if (transforms.length > 0) passes.push(this.compile(transforms, output))
-  console.log('PASSES', passes)
+  //console.log('PASSES', passes)
 
   return passes
 }

commit ce3c74549cd35bd1aae217eed993a786a411cd66

fix: Fix prev() function to correctly reference previous frame buffer
The prev() function was not working due to incorrect ping-pong buffer
indexing. It was returning the current frame buffer instead of the
previous one.

This bug was introduced in commit 9b4e8e9 (2020-04-01) when removing
the render passes system. The correct index calculation was commented
out but never uncommented when switching to the single-pass system.

Changes:
- Uncommented and fixed the index calculation in prevBuffer uniform
- Changed from `self.pingPongIndex` to `index` (opposite buffer)
- This matches the correct implementation in getTexture() method

Now feedback effects using prev() will work properly.

🤖 Generated with [Claude Code](https://claude.com/claude-code)

Co-Authored-By: Claude <noreply@anthropic.com>

dist/hydra-synth.js
@@ -4469,10 +4469,10 @@ Output.prototype.render = function (passes) {
   var self = this;
   var uniforms = Object.assign(pass.uniforms, {
     prevBuffer: () => {
-      //var index = this.pingPongIndex ? 0 : 1
-      //   var index = self.pingPong[(passIndex+1)%2]
+      var index = self.pingPongIndex ? 0 : 1; //   var index = self.pingPong[(passIndex+1)%2]
       //  console.log('ping pong', self.pingPongIndex)
-      return self.fbos[self.pingPongIndex];
+
+      return self.fbos[index];
     }
   });
   self.draw = self.regl({

src/output.js
@@ -95,10 +95,10 @@ Output.prototype.render = function (passes) {
   //console.log('pass', pass, this.pingPongIndex)
   var self = this
       var uniforms = Object.assign(pass.uniforms, { prevBuffer:  () =>  {
-             //var index = this.pingPongIndex ? 0 : 1
+             var index = self.pingPongIndex ? 0 : 1
           //   var index = self.pingPong[(passIndex+1)%2]
           //  console.log('ping pong', self.pingPongIndex)
-            return self.fbos[self.pingPongIndex]
+            return self.fbos[index]
           }
         })
 